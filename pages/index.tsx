36fed9d866e7bcc4c730e1b256c900d0e2aada03
poohoot <poohoot.dev@gmail.com>
2022-05-17 18:00:35 +0900

    TailwindCSS 설치

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <></>;
+  return <h1 className="text-3xl font-bold underline">TailwindCSS!</h1>;
 };
 
 export default Home;
